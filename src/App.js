214b2435ade93a86a387c94e9dd37918e40f1fc1
Sandeep Kumar <sandeepkumar.sk5.ss55@gmail.com>
2021-11-14 01:57:53 +0530

    install react-icons package and use some icons

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import { IconContext } from 'react-icons';
+import { FaReact } from 'react-icons/fa'
+import { MdAlarm } from 'react-icons/md'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <IconContext.Provider value={{ color: 'green', size: '10rem' }}>
+      <div className="App">
+        <FaReact  />
+        <MdAlarm />
+        {/*  below icons override property */}
+        <FaReact color='purple' size='20rem' />
+        <MdAlarm color='purple' size='20rem' />
+      </div>
+    </IconContext.Provider>
   );
 }
 
