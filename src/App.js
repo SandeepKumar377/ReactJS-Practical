7cf009182768c398cc7b931b0ae1c77cd110bdc3
Sandeep Kumar <sandeepkumar.sk5.ss55@gmail.com>
2021-11-14 16:15:56 +0530

    Component render as a Custom Toast Notification

--- src/App.js
+++ src/App.js
@@ -5,16 +5,24 @@ import { MdAlarm } from 'react-icons/md'
 import { toast } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 
+const CustomToast=({closeToast})=>{
+  return(
+    <div>
+      Something went wrong!
+      <button onClick={closeToast} >Close</button>
+    </div>
+  )
+}
 toast.configure()
 function App() {
 
   const toastDemo=()=>{
     toast('Basic Notification!')
-    toast.success('Success Notification!', {position: toast.POSITION.TOP_CENTER, autoClose:3000})
+    toast.success('Success Notification!', {position: toast.POSITION.TOP_CENTER, autoClose:false})
     toast.warn('Warning Notification!', {position: toast.POSITION.BOTTOM_LEFT, autoClose:4000})
     toast.info('Info Notification!', {position: toast.POSITION.BOTTOM_RIGHT, autoClose:5000})
     toast.error('Error Notification!', {position: toast.POSITION.BOTTOM_CENTER, autoClose:6000})
-    toast('Basic Notification!', {position: toast.POSITION.TOP_LEFT, autoClose:false})
+    toast(<CustomToast/>, {position: toast.POSITION.TOP_LEFT, autoClose:false})
   }
 
   return (
