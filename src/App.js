de4bf8dfce616da373b63b24d6b9e69f245f3bb3
Sandeep Kumar <sandeepkumar.sk5.ss55@gmail.com>
2021-11-14 16:07:46 +0530

    Toast Notifications Demo

--- src/App.js
+++ src/App.js
@@ -2,18 +2,38 @@ import './App.css';
 import { IconContext } from 'react-icons';
 import { FaReact } from 'react-icons/fa'
 import { MdAlarm } from 'react-icons/md'
+import { toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
+toast.configure()
 function App() {
+
+  const toastDemo=()=>{
+    toast('Basic Notification!')
+    toast.success('Success Notification!', {position: toast.POSITION.TOP_CENTER, autoClose:3000})
+    toast.warn('Warning Notification!', {position: toast.POSITION.BOTTOM_LEFT, autoClose:4000})
+    toast.info('Info Notification!', {position: toast.POSITION.BOTTOM_RIGHT, autoClose:5000})
+    toast.error('Error Notification!', {position: toast.POSITION.BOTTOM_CENTER, autoClose:6000})
+    toast('Basic Notification!', {position: toast.POSITION.TOP_LEFT, autoClose:false})
+  }
+
   return (
-    <IconContext.Provider value={{ color: 'green', size: '10rem' }}>
-      <div className="App">
-        <FaReact  />
-        <MdAlarm />
-        {/*  below icons override property */}
-        <FaReact color='purple' size='20rem' />
-        <MdAlarm color='purple' size='20rem' />
-      </div>
-    </IconContext.Provider>
+    <div>
+      <hr />
+      <h1>Icons Demo</h1>
+      <IconContext.Provider value={{ color: 'green', size: '5rem' }}>
+        <div className="App">
+          <FaReact />
+          <MdAlarm />
+          {/*  below icons override property */}
+          <FaReact color='purple' size='10rem' />
+          <MdAlarm color='purple' size='10rem' />
+        </div>
+      </IconContext.Provider>
+      <hr />
+      <h1>Toast Notifications Demo</h1>
+      <button onClick={toastDemo} >Click here...</button>
+    </div>
   );
 }
 
